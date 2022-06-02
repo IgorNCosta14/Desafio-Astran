aebaf63c18f670304863976f230bae7cbc1e7984
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-01 23:46:50 -0300

    Creating stocks historic routes

--- src/shared/http/routes/stocks.routes.ts
+++ src/shared/http/routes/stocks.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { FindStockByNameController } from "../../../modules/stocks/useCases/FindStockByNameController";
+import { FindStockByNameController } from "../../../modules/stocks/useCases/FindStockByName/FindStockByNameController";
+import { GetStockHistoricController } from "../../../modules/stocks/useCases/GetStockHistoric/GetStockHistoricController";
 
 const stocksRoutes = Router();
 
 const findStockByNameController = new FindStockByNameController()
+const getStockHistoricController = new GetStockHistoricController()
 
 stocksRoutes.get("/:stock_name/quote", findStockByNameController.handle);
+stocksRoutes.get("/:stock_name/history", getStockHistoricController.handle)
 
 export { stocksRoutes };
