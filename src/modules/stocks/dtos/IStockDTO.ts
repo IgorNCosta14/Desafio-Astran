9ff1c357f933ee3ce645612120fdf1b52ce27f01
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-02 16:01:06 -0300

    Update IStockDTO.ts

--- src/modules/stocks/dtos/IStockDTO.ts
+++ src/modules/stocks/dtos/IStockDTO.ts
@@ -9,3 +9,43 @@ export interface IGetHistoricDTO {
     from: string, 
     to: string,
 }
+
+export interface IMultipleQuotesDTO {
+    stock_name: string,
+    stocks: []
+}
+
+interface timeSeries {
+    '1. open': string,
+    '2. high': string,
+    '3. low': string,
+    '4. close': string,
+    '5. volume': string
+}
+
+export interface IStockHistoricResponseDTO {
+    'Meta Data': {
+      '1. Information': string,
+      '2. Symbol': string,
+      '3. Last Refreshed': string,
+      '4. Output Size': string,
+      '5. Time Zone': string,
+    },
+    'Time Series (Daily)': timeSeries
+}
+
+export interface IFindStockResponseDTO {
+    'Global Quote': {
+        '01. symbol': string,
+        '02. open': string,
+        '03. high': string,
+        '04. low': string,
+        '05. price': string,
+        '06. volume': string,
+        '07. latest trading day': string,
+        '08. previous close': string,
+        '09. change': string,
+        '10. change percent': string
+    }
+}
+
