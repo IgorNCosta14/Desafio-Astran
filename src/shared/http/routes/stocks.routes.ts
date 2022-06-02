e4d7005d660584dedf1e7eda86f2a4e75117e8cf
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-02 19:54:33 -0300

    Creating compare route

--- src/shared/http/routes/stocks.routes.ts
+++ src/shared/http/routes/stocks.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CompareStocksController } from "../../../modules/stocks/useCases/CompareStocks/CompareStocksController";
 import { FindStockByNameController } from "../../../modules/stocks/useCases/FindStockByName/FindStockByNameController";
+import { GainsProjectionController } from "../../../modules/stocks/useCases/GainsProjection/GainsProjectionController";
 import { GetStockHistoricController } from "../../../modules/stocks/useCases/GetStockHistoric/GetStockHistoricController";
 
 const stocksRoutes = Router();
@@ -8,9 +9,11 @@ const stocksRoutes = Router();
 const findStockByNameController = new FindStockByNameController();
 const getStockHistoricController = new GetStockHistoricController();
 const compareStocksController = new CompareStocksController();
+const gainsProjectionController = new GainsProjectionController();
 
 stocksRoutes.get("/:stock_name/quote", findStockByNameController.handle);
 stocksRoutes.get("/:stock_name/history", getStockHistoricController.handle);
 stocksRoutes.get("/:stock_name/compare", compareStocksController.handle);
+stocksRoutes.get("/:stock_name/gains", gainsProjectionController.handle);
 
 export { stocksRoutes };
