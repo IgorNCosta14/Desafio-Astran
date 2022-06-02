65d7ff1fe25237bad3fee7b04c78890918d25997
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-02 16:01:36 -0300

    Update GetStockHistoricUseCase.ts

--- src/modules/stocks/useCases/GetStockHistoric/GetStockHistoricUseCase.ts
+++ src/modules/stocks/useCases/GetStockHistoric/GetStockHistoricUseCase.ts
@@ -2,6 +2,14 @@ import { inject, injectable } from "tsyringe";
 import { IStocksProvider } from "../../../../shared/container/SocksProvider/IStocksProvider";
 import { IGetHistoricDTO } from "../../dtos/IStockDTO";
 
+interface Pricing {
+    opening: string,
+    low: string,
+    high: string,
+    closing: string,
+    pricedAt: string
+}
+
 @injectable()
 export class GetStockHistoricUseCase {
     constructor(
@@ -10,8 +18,32 @@ export class GetStockHistoricUseCase {
     ) {}
 
     async execute({stock_name, from, to}: IGetHistoricDTO) {
-        const stockHistoric = this.stocksProvider.fetchHistoric({stock_name, from, to})
+        const stockHistoric = await this.stocksProvider.fetchHistoric(stock_name)
 
-        return stockHistoric;
+        console.log(stockHistoric)
+
+        const stockHistoricData = {
+            name: stockHistoric["Meta Data"]["2. Symbol"],
+            prices: []
+        }
+
+        const priceArray = Object.entries(stockHistoric["Time Series (Daily)"])
+        
+        const filteredByData = priceArray.filter(([data]) => data >= from && data <= to);
+
+        for (const value of filteredByData) {
+
+            const pricing: Pricing = {
+                opening: value[1]['1. open'],
+                low: value[1]['3. low'],
+                high: value[1]['2. high'],
+                closing: value[1]['4. close'],
+                pricedAt: value[0]
+            }
+            stockHistoricData.prices.push(pricing)
+        }
+
+        return stockHistoricData;
+
     }
 }
