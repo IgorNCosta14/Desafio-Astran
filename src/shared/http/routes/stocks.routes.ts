9a45e06cdc67fac7a33d097e1cf6df073f8f1a33
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-02 14:59:22 -0300

    Update stocks.routes.ts

--- src/shared/http/routes/stocks.routes.ts
+++ src/shared/http/routes/stocks.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import { CompareStocksController } from "../../../modules/stocks/useCases/CompareStocks/CompareStocksController";
 import { FindStockByNameController } from "../../../modules/stocks/useCases/FindStockByName/FindStockByNameController";
 import { GetStockHistoricController } from "../../../modules/stocks/useCases/GetStockHistoric/GetStockHistoricController";
 
 const stocksRoutes = Router();
 
-const findStockByNameController = new FindStockByNameController()
-const getStockHistoricController = new GetStockHistoricController()
+const findStockByNameController = new FindStockByNameController();
+const getStockHistoricController = new GetStockHistoricController();
+const compareStocksController = new CompareStocksController();
 
 stocksRoutes.get("/:stock_name/quote", findStockByNameController.handle);
-stocksRoutes.get("/:stock_name/history", getStockHistoricController.handle)
+stocksRoutes.get("/:stock_name/history", getStockHistoricController.handle);
+stocksRoutes.get("/:stock_name/compare", compareStocksController.handle);
 
 export { stocksRoutes };
