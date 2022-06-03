dff85d33a799e4aec06f823b80010e9a0a8d8b77
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-02 22:06:31 -0300

    Update FindStockByNameUseCase.ts

--- src/modules/stocks/useCases/FindStockByName/FindStockByNameUseCase.ts
+++ src/modules/stocks/useCases/FindStockByName/FindStockByNameUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe"
 import { IStocksProvider } from "../../../../shared/container/SocksProvider/IStocksProvider"
+import { AppError } from "../../../../shared/errors/AppError";
 import { IFindStockDTO } from "../../dtos/IStockDTO";
 
 @injectable()
@@ -9,17 +10,15 @@ export class FindStockByNameUseCase {
         private stocksProvider: IStocksProvider
     ) {}
 
-    async execute(stock_name: string): Promise<IFindStockDTO>{   
- 
-        const stock = await this.stocksProvider.fetchQuote(stock_name)
+    async execute(stock_name: string): Promise<IFindStockDTO>{
+            const stock = await this.stocksProvider.fetchQuote(stock_name)
 
-        const stockData = {
-            name: stock["Global Quote"]["01. symbol"],
-            lastPrice: parseFloat(stock["Global Quote"]["05. price"]),
-            pricedAt: (new Date()).toISOString()
-        }
-        
-        return stockData;
+            const stockData = {
+                name: stock["Global Quote"]["01. symbol"],
+                lastPrice: parseFloat(stock["Global Quote"]["05. price"]),
+                pricedAt: (new Date()).toISOString()
+            }
 
+            return stockData;
     }
 }
