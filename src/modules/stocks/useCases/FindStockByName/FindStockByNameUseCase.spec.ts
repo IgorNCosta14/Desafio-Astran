d4ca93089d46fdac16801506f9b1fcfd2292e5c8
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-03 01:55:08 -0300

    Find stock by name test

--- src/modules/stocks/useCases/FindStockByName/FindStockByNameUseCase.spec.ts
+++ src/modules/stocks/useCases/FindStockByName/FindStockByNameUseCase.spec.ts
@@ -1,1 +1,25 @@
-describe("Find stock by name"{}) 
+import { strict } from "assert";
+import { StocksProvider } from "../../../../shared/container/StocksProvider/implementations/StocksProvider";
+import { FindStockByNameUseCase } from "./FindStockByNameUseCase"
+
+let stocksProvider: StocksProvider;
+let findStockByNameUseCase: FindStockByNameUseCase;
+
+
+describe("Find stock by name", () => {
+    beforeEach(() => {
+        stocksProvider = new StocksProvider();
+        findStockByNameUseCase = new FindStockByNameUseCase(stocksProvider);
+    })
+
+    it("should be able to get data of the stock", async () => {
+        const stock = await findStockByNameUseCase.execute("IBM")
+
+        expect(stock).toHaveProperty("name")
+        expect(stock.name).toBe("IBM")
+        expect(stock).toHaveProperty("pricedAt")
+        expect(typeof stock.pricedAt).toBe("string")
+        expect(stock).toHaveProperty("lastPrice")
+        expect(typeof stock.lastPrice).toBe("number")
+    })
+}) 
